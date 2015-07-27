8b902eea21454d4fb1bb8ca679e479883084f36a
Byron Hammond <byronester@gmail.com>
2015-07-27 16:08:34 +0800

    search-11btn.py now produces a PDF link to the scanned newspaper page

--- bin/search-11btn.py
+++ bin/search-11btn.py
@@ -1,10 +1,35 @@
 #!/usr/bin/env python3
 
+import requests
 import os
 import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '../lib'))
+from trove.request import Request as TroveRequest
+from trove.request import NewspaperTitlesRequest as TroveNewspaperTitlesRequest
 
-from lib.trove.request import Request as TroveRequest
+TROVE_API_KEY = os.getenv('TROVE_API_KEY', '')
 
-request = TroveRequest()
+newspapers = TroveNewspaperTitlesRequest(TROVE_API_KEY)
 
+trove = TroveRequest(TROVE_API_KEY)
+query_params = {
+    'l-illustrated': 'Y',                       # contains images
+    'l-title': newspapers.get_ids(state='wa'),  # restrict to given newspapers
+    'reclevel': 'full',                         # full detail records
+    's': 0,                                     # start
+    'n': 50,                                    # num of results
+}
+trove.get(query='11th Battalion date:[1914 TO 1921]', params=query_params)
+
+print("Request URL: %s" % trove.response.url)
+if trove.response.status_code != requests.codes.ok:
+    print("Error: Problem processing request")
+else:
+    # print("Response: %s" % trove.response.text)
+    response = trove.response.json()
+    # print("Response Records: %s" % response['response'])
+    print("%s Records (Total)" % response['response']['zone'][0]['records']['total'])
+    for article in response['response']['zone'][0]['records']['article']:
+        print("Article")
+        print("\tURL: %s" % article['url'])
+        print("\tPDF: %s" % article['pdf'])
--- lib/trove/request.py
+++ lib/trove/request.py
@@ -20,3 +20,55 @@ class Request:
             request_params.update(params)
         request_params['q'] = query
         self.response = requests.get(self.API_URL_BASE, params=request_params)
+
+
+class NewspaperTitlesRequest:
+    API_URL_BASE = 'http://api.trove.nla.gov.au/newspaper/titles'
+    API_ENCODING = 'json'
+
+    def __init__(self, api_key=''):
+        self.params = {
+            'key': api_key,
+            'encoding': self.API_ENCODING
+        }
+        self.response = None       # response object from requests.get
+        self.response_json = None  # python dict from calling .json()
+
+    def get_ids(self, state='wa'):
+        params = {
+            'state': state
+        }
+        params.update(self.params)
+        self.response = requests.get(self.API_URL_BASE, params=params)
+        self.response_json = self.response.json()
+
+        ids = []
+        newspapers = self.response_json['response']['records']['newspaper']
+        for newspaper in newspapers:
+            ids.append(newspaper['id'])
+        return ids
+
+    def url_params(self, newspaper_ids=[]):
+        url_params = ""
+        for newspaper_id in newspaper_ids:
+            url_params += "l-title={0}&".format(newspaper_id)
+        url_params.rstrip('&')
+        return url_params
+
+    def get_str(self, state='wa'):
+        params = {
+            'state': state
+        }
+        params.update(self.params)
+
+        self.response = requests.get(self.API_URL_BASE, params=params)
+        if self.response.status_code != requests.codes.ok:
+            return None
+        else:
+            # print("Response: %s" % r.text)
+            self.response_json = self.response.json()
+            newspaper_ids = self.get_ids()
+            # print("Newspaper Ids: %s" % newspaper_ids)
+            # print("Response Records: %s" % response['response']['records']['newspaper'])
+            url_params = self.url_params(newspaper_ids)
+            return url_params
