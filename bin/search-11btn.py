83a719f9fafb6230ca1e453b8f3a782562394813
Byron Hammond <byronester@gmail.com>
2015-08-05 16:29:49 +0800

    Addede code to get the pictures associated with the articles found in a search

--- bin/search-11btn.py
+++ bin/search-11btn.py
@@ -1,12 +1,36 @@
 #!/usr/bin/env python3
 
 import requests
+from urllib.parse import urlparse
+from bs4 import BeautifulSoup
+import shutil
 import os
 import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '../lib'))
 from trove.request import Request as TroveRequest
 from trove.request import NewspaperTitlesRequest as TroveNewspaperTitlesRequest
 
+
+def image_path(url):
+    parsed_url = urlparse(url)
+    sections = parsed_url.path.split('/')
+    image_name = sections[-1]
+    path = '/tmp/' + image_name
+    return path
+
+
+def download_image(url):
+    r = requests.get(url, stream=True)
+    if r.status_code != requests.codes.ok:
+        raise RuntimeError("{0} Error downloading image: {1}".format(r.status_code, url))
+    else:
+        path = image_path(url)
+        with open(path, 'wb') as f:
+            r.raw.decode_content = True
+            shutil.copyfileobj(r.raw, f)
+
+
+TROVE_URL_BASE = 'http://trove.nla.gov.au'
 TROVE_API_KEY = os.getenv('TROVE_API_KEY', '')
 
 newspapers = TroveNewspaperTitlesRequest(TROVE_API_KEY)
@@ -30,11 +54,15 @@ else:
     # print("Response Records: %s" % response['response'])
     print("%s Records (Total)" % response['response']['zone'][0]['records']['total'])
     for article in response['response']['zone'][0]['records']['article']:
-        image_url = 'http://trove.nla.gov.au/ndp/del/printArticleJpg/{0}/3?print=n'.format(article['id'])
+        image_url = 'http://trove.nla.gov.au/ndp/del/printArticleJpg/{0}/3'.format(article['id'])
         print("Article")
         print("\tURL: %s" % article['url'])
         print("\tPDF: %s" % article['pdf'])
         print("\tImage: %s" % image_url)
 
         r = requests.get(image_url)
-        # if r.status_code == requests.codes.ok:
+        if r.status_code == requests.codes.ok:
+            html_doc = r.text
+            soup = BeautifulSoup(html_doc, 'html.parser')
+            image = soup.select('img#articleImg')[0]
+            download_image(TROVE_URL_BASE + image['src'])
